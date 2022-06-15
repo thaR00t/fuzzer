bd259a8001d73c88f04ca94d608a25cdbbc04dc2
ShortCu7 <87804260+thaR00t@users.noreply.github.com>
2022-06-15 12:02:10 +0200

    Update dirfuzzer.py

--- dirfuzzer.py
+++ dirfuzzer.py
@@ -13,13 +13,14 @@ epilog=textwrap.dedent('''EXAMPLE:\n
 dirfuzzer.py -u http://example.com -w <wordlist> # Simple fuzzin
 dirfuzzer.py -u http://example.com -w <wordlist> -x .txt # Add extension
 dirfuzzer.py -u http://example.com -w <wordlist> -o output.txt # Save the fuzzing's output
+dirfuzzer.py -b banner # Show the banner :)
 '''))
 
 parser.add_argument('-u','--url',type=str, help='Specified an url.')
 parser.add_argument('-w','--wordlist', help='Insert a wordlists.')
 parser.add_argument('-x','--extension',type=str,help='Select an extension for the fuzzin')
 parser.add_argument('-o','--output',help='Save the output into a file.')
-parser.add_argument('-b','--banner',help='If you want you can spawn the banner')
+parser.add_argument('-b','--banner',help='Print the banner')
 args = parser.parse_args()
 
 
@@ -36,31 +37,32 @@ try:
     if ban:
         print(display())
     else:
-        if url and wlist:
-            print(display())
-            print(status())
-            wordlistline = open(wlist, 'r').readlines()
-            #for loop to analyze the wordlist
-            for i in range(0,len(wordlistline)):                                                                
-                enumeration=wordlistline[i].replace("\n",(ext or ""))
+        if __name__ == '__main__':
+            if url and wlist:
+                print(display())
+                print(status())
+                wordlistline = open(wlist, 'r').readlines()
+                #for loop to analyze the wordlist
+                for i in range(0,len(wordlistline)):                                                                
+                    enumeration=wordlistline[i].replace("\n",(ext or ""))
 
-                #Send a request                                                                                                                                                   
-                r = requests.get(url+"/"+enumeration)                                                           
-                #Check the status of the page if is 404 is not available then don't print it                                                                                           
-                if  r.status_code != 404:                                                                       
-                    output = ("         ("+str(r.status_code)+")        "+url+"/"+enumeration)                                         
+                    #Send a request                                                                                                                                                   
+                    r = requests.get(url+"/"+enumeration)                                                           
+                    #Check the status of the page if is 404 is not available then don't print it                                                                                           
+                    if  r.status_code != 404:                                                                       
+                        output = ("         ("+str(r.status_code)+")        "+url+"/"+enumeration)                                         
 
-                    print(f"[bold orange_red1]{output}[/bold orange_red1]")
-                    time.sleep(0.1)
-                    if out:                
-                        outfile = (str(r.status_code)+"   "+url+'/'+enumeration)
-                        with open(out,'a') as f:
-                            f.write(outfile+"\n")
-                            f.close()
+                        print(f"[bold orange_red1]{output}[/bold orange_red1]")
+                        time.sleep(0.1)
+                        if out:                
+                            outfile = (str(r.status_code)+"   "+url+'/'+enumeration)
+                            with open(out,'a') as f:
+                                f.write(outfile+"\n")
+                                f.close()
 
-        else:
+            else:
 
-            print(help())
+                print(help())
 
 except KeyboardInterrupt:
         print("[bold red] [!]Script interrupt by user[!][/bold red]")
